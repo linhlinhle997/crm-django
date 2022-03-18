d88522fe46b982e9a110e97c6b99ec5d45c6e76d
linhprimelabo <i.linh@primelabo.com.vn>
2022-03-18 15:52:40 +0700

    custom user model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# class Lead(models.Model):
+#     SOURCE_CHOICES = (
+#         ('Youtube', 'Youtube'),
+#         ('Google', 'Google'),
+#         ('Newsletter', 'Newsletter')
+#     )
+#     first_name = models.CharField(max_length=20)
+#     last_name = models.CharField(max_length=20)
+#     age = models.IntegerField(default=0)
+#     phoned = models.BooleanField(default=False)
+#     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+#     profile_picture = models.ImageField(blank=True, null=True)
+#     special_files = models.FileField(blank=True, null=True)
+
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    # every lead have 1 Agent 
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)  
+
+class Agent(models.Model):
+    # every lead have 1 User 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
